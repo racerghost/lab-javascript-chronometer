52f8059fb503b3b015ec8a7ea725fbdb5a8003da
Carlos Bausá Carmona <carlos@MacBook-Pro-de-Carlos.local>
2021-03-05 18:59:19 +0100

    Done JS Part

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,93 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=parseInt(0);
+    this.intervalId=parseInt(0);
   }
+  
   startClick(callback) {
-    // ... your code goes here
+ 
+    let time=this.currentTime;   
+  
+    setInterval(() => {     
+      this.currentTime++;  
+    }, 1000);
+  
+  
   }
+
+
+
   getMinutes() {
-    // ... your code goes here
+    if (isNaN(this.currentTime)){
+      return 0;
+    } else {
+      return Math.trunc((this.currentTime/60));
+    }
   }
+
   getSeconds() {
-    // ... your code goes here
+
+    if (isNaN(this.currentTime)){
+      return 0;
+    } else if (this.currentTime>60){
+      return this.currentTime % 60      
+    } else { 
+      return this.currentTime;
+    }
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+
+  twoDigitsNumber() {  
+
+    let min=String(this.getMinutes());
+    let sec=String(this.getSeconds());
+
+    if (min.length < 2){
+      min='0'+min;
+      return min;
+    } else {
+      return min;
+    }
+
+    if (sec.length < 2){    // no entiendo que pide exactamente la función
+                            // es decir, si, pero ¿no deberia entrar algo como parámetro para convertirlo a 2digit?
+      sec='0'+sec;
+      return sec;
+    } else {
+      return sec;
+    }   
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval();
   }
+
   resetClick() {
-    // ... your code goes here
+    clearInterval();
+    this.currentTime=0;
   }
+
   splitClick() {
-    // ... your code goes here
+
+    let min = String(this.getMinutes());
+    let sec = String(this.getSeconds());
+
+    if (min < 10 && sec < 10) {
+      return('0'+min+':'+'0'+sec)
+    } else if (min < 10 && sec > 10) {
+      return('0'+min+':'+sec);
+    } else if (min > 10 && sec < 10) {
+      return(min+':0'+sec);
+    } else {
+      return(min+':'+sec);
+    }
   }
 }
+
+let pp=new Chronometer;
+
+ pp.startClick();
+
+ pp.getMinutes();
+
+ pp.twoDigitsNumber();
